e6c76ebadc63edcf9b43cbf8cb6a44f4f6a9fc1f
zaura <zaura@DESKTOP-NUA7KUI>
2023-10-30 10:42:54 -0700

    Task1 done

--- src/main/java/org/example/task1/Task1.java
+++ src/main/java/org/example/task1/Task1.java
@@ -2,4 +2,11 @@ package org.example.task1;
 
 public class Task1 {
 
+    static public double solve(int x, int y) {
+        double denominator = 1 + Math.pow(Math.sin(x + y), 2);
+        double numerator = 2 + Math.abs(x - (2 * x / (1 + x^2 * y^2)));
+
+        return numerator / denominator;
+    }
+
 }
