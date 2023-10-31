f5caff21f7838269da43849e6adaf72d525525df
zaura <zaura@DESKTOP-NUA7KUI>
2023-10-31 04:58:07 -0700

    Task13 done

---
+++ src/main/java/org/example/task13/Book.java
@@ -0,0 +1,8 @@
+package org.example.task13;
+
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+    private static int edition;
+}
---
+++ src/main/java/org/example/task13/ProgrammerBook.java
@@ -0,0 +1,40 @@
+package org.example.task13;
+
+public class ProgrammerBook {
+    private String programmingLanguage;
+    private int programmingLevel;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        ProgrammerBook otherBook = (ProgrammerBook) obj;
+
+        return programmingLanguage.equals(otherBook.programmingLanguage) &&
+                programmingLevel == otherBook.programmingLevel;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = programmingLanguage.hashCode();
+        result = 33 * result + programmingLevel;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ProgrammerBook{" +
+                "programmingLanguage='" + programmingLanguage + '\'' +
+                ", programmingLevel=" + programmingLevel +
+                '}';
+    }
+
+}
+
+
