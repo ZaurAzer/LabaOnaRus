54fbb27d4f750a642b4a00137416d30783de7521
zaura <zaura@DESKTOP-NUA7KUI>
2023-10-31 08:59:32 -0700

    Task15 done

---
+++ src/main/java/org/example/task15/Task15.java
@@ -0,0 +1,17 @@
+package org.example.task15;
+
+import org.example.task12.Task12;
+
+public class Task15 implements Comparable<Task12> {
+    private String isbn;
+    private String title;
+    private String author;
+    private int price;
+    private static int edition;
+
+    @Override
+    public int compareTo(Task12 o) {
+        return this.isbn.compareTo(o.bookISBN);
+    }
+
+}
